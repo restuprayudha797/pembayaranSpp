b3199d036c171056791754b26ec2e1b6ce377f13
restuprayudha797 <yudariau1@gmail.com>
2023-02-09 17:31:54 +0700

    added view and action for validati level users

--- config/function.php
+++ config/function.php
@@ -38,27 +38,21 @@ function Registrasi()
     $password = $_POST['password'];
     // akhir ambil data dari pengguna
 
+
     $user = query("SELECT * FROM user WHERE username = '$username'");
 
     if (!$user) {
+
+        // enkripsi password
         $password = password_hash($password, PASSWORD_DEFAULT);
+        // akhir enkripsi
+
 
         query("INSERT INTO `user`(`id_users`, `username`, `password`, `aktif`) VALUES ('','$username','$password', 1)");
     } else {
 
         echo 'username sudah tersedia';
     }
-    // enkripsi password
-
-
-
-    // akhir enkripsi
-
-
-
-
-
-
 }
 // akhir function Registrasi
 
@@ -67,30 +61,41 @@ function login()
 {
 
 
+
     $username = $_POST['username'];
     $password = $_POST['password'];
 
     global $conn;
 
-    $user = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
+    $user = mysqli_query($conn, "SELECT * FROM petugas WHERE username = '$username'");
 
     if (mysqli_num_rows($user)) {
 
 
         $row = mysqli_fetch_assoc($user);
 
+
         if (password_verify($password, $row['password'])) {
 
 
+            $_SESSION['login'] = true;
+            $_SESSION['id_petugas'] = $row['id_petugas'];
 
+            echo '<script>alert("Login Berhasil"); window.location="login.php"</script>';
 
-            header("Location: index.php");
+            exit;
         } else {
 
-            echo 'password tidak sesuai';
-            header("Location: login.php");
-            die;
+            echo '<script>alert("Password tidak sesuai silahkan cobalagi"); window.location="login.php"</script>';
+
+
+            exit;
         }
+    } else {
+
+        echo '<script>alert("Username tidak dapat ditemukan silahkan cobalagi"); window.location="login.php"</script>';
+
+        exit;
     }
 
 
--- login.php
+++ login.php
@@ -1,6 +1,11 @@
 <?php
+session_start();
+if (isset($_SESSION['login'])) {
 
+    header("Location: index.php");
+}
 require 'config/function.php';
+
 
 if (isset($_POST['tLogin'])) {
 
@@ -23,56 +28,56 @@ if (isset($_POST['tLogin'])) {
     <link rel="stylesheet" href="assets/css/bootstrap.css">
 
     <style>
-    .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-    }
-
-    @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-            font-size: 3.5rem;
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
         }
-    }
 
-    .b-example-divider {
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-    }
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
 
-    .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-    }
+        .b-example-divider {
+            height: 3rem;
+            background-color: rgba(0, 0, 0, .1);
+            border: solid rgba(0, 0, 0, .15);
+            border-width: 1px 0;
+            box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+        }
 
-    .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-    }
+        .b-example-vr {
+            flex-shrink: 0;
+            width: 1.5rem;
+            height: 100vh;
+        }
 
-    .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-    }
+        .bi {
+            vertical-align: -.125em;
+            fill: currentColor;
+        }
 
-    .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-    }
+        .nav-scroller {
+            position: relative;
+            z-index: 2;
+            height: 2.75rem;
+            overflow-y: hidden;
+        }
+
+        .nav-scroller .nav {
+            display: flex;
+            flex-wrap: nowrap;
+            padding-bottom: 1rem;
+            margin-top: -1px;
+            overflow-x: auto;
+            text-align: center;
+            white-space: nowrap;
+            -webkit-overflow-scrolling: touch;
+        }
     </style>
 
 
@@ -107,8 +112,7 @@ if (isset($_POST['tLogin'])) {
                 <label for="username">Username</label>
             </div>
             <div class="form-floating mt-3">
-                <input type="password" class="form-control" id="password" name="password"
-                    placeholder="Masukkan Password">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Masukkan Password">
                 <label for="password">Password</label>
             </div>
 
